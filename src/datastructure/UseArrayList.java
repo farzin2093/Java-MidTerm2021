3d87b21c2388faea83ba2bd2571c563e6ca07a53
farzin93 <farzinsheikhlive@gmail.com>
2021-05-02 21:30:47 -0700

    initial commit

--- src/datastructure/UseArrayList.java
+++ src/datastructure/UseArrayList.java
@@ -1,15 +1,39 @@
-package datastructure;
 
-public class UseArrayList {
+    package datastructure;
 
-    public static void main(String[] args) {
-        /*
-         * Demonstrate how to use ArrayList that includes add,peek,remove,retrieve elements.
-         * Use For Each loop and while loop with Iterator to retrieve data.
-         * Store all the sorted data into one of the databases.
-         *
-         */
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ListIterator;
 
-    }
+        public class UseArrayList {
 
-}
+            public static void main(String[] args) {
+                /*
+                 * Demonstrate how to use ArrayList that includes add,peek,remove,retrieve elements.
+                 * Use For Each loop and while loop with Iterator to retrieve data.
+                 * Store all the sorted data into one of the databases.
+                 *
+                 */
+                ArrayList<Integer> numberList = new ArrayList<>();
+                numberList.add(100);
+                numberList.add(200);
+                numberList.add(300);
+
+                //numberList.remove(2);
+                //numberList.get(0);
+
+                //iterator
+                Iterator iterator = numberList.iterator();
+                while (iterator.hasNext()) {
+                    System.out.println(iterator.next());
+
+
+
+                }
+
+
+            }
+        }
+
+
+
