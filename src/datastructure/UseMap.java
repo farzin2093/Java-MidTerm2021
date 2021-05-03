44c2ad44a5784062c7e04d3874bb7ee3a4e9fdb4
farzin93 <farzinsheikhlive@gmail.com>
2021-05-02 18:04:27 -0700

    initial commit

---
+++ src/datastructure/UseMap.java
@@ -0,0 +1,58 @@
+package datastructure;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+public class UseMap {
+
+    public static void main(String[] args) {
+        /*
+         * Demonstrate how to use Map that includes storing and retrieving elements.
+         * Add List<String> into a Map. Like, Map<String, List<string>> list = new HashMap<String, List<String>>();
+         * Use For Each loop and while loop with Iterator to retrieve data.
+         *
+         * Use MySql Database to store data and retrieve data.
+         */
+
+
+
+
+
+
+                HashMap<Integer, String> usemap = new HashMap<Integer, String>();
+
+
+                usemap.put(12, "Meron");
+                usemap.put(2, "Asgedom");
+                usemap.put(7, "Weds");
+                usemap.put(49, "Zann");
+                usemap.put(3, "Sehum");
+
+                /* Display content using Iterator*/
+                Set set = usemap.entrySet();
+                Iterator iterator = set.iterator();
+                while(iterator.hasNext()) {
+                    Map.Entry mentry = (Map.Entry)iterator.next();
+                    System.out.print("key is: "+ mentry.getKey() + " & Value is: ");
+                    System.out.println(mentry.getValue());
+                }
+
+                /* Get values based on key*/
+                String var= usemap.get(2);
+                System.out.println("Value at index 2 is: "+var);
+
+                /* Remove values based on key*/
+                usemap.remove(3);
+                System.out.println("Map key and values after removal:");
+                Set set2 = usemap.entrySet();
+                Iterator iterator2 = set2.iterator();
+                while(iterator2.hasNext()) {
+                    Map.Entry mentry2 = (Map.Entry)iterator2.next();
+                    System.out.print("Key is: "+mentry2.getKey() + " & Value is: ");
+                    System.out.println(mentry2.getValue());
+                }
+
+            }
+        }
+
